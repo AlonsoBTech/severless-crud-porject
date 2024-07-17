eb17df302fca1b2c9c031375f9fe12d7248c4996
Alonso Braithwaite <160416175+AlonsoBTech@users.noreply.github.com>
2024-07-16 21:10:19 -0400

    Update image_scaler.py

--- image_scaler.py
+++ image_scaler.py
@@ -1,20 +1,19 @@
 import json
 import boto3
 from PIL import Image
-import io.BytesIO
- 
+import io
+
 s3_client = boto3.client('s3')
 dest_bucket_name = 'aeb-image-scaler-destination'
 
 def lambda_handler(event, context):
-
     try:
-        # Extract relevant information
+        # Retrieving information about the uploaded image
         s3_event = event['Records'][0]['s3']
         bucket_name = s3_event['bucket']['name']
         object_key = s3_event['object']['key']
-
-        # Downloading the image from the S3 origin bucket
+        
+        # Downloading the image from the S3 bucket
         response = s3_client.get_object(Bucket=bucket_name, Key=object_key)
         object_content = response['Body'].read()
 
@@ -29,18 +28,18 @@ def lambda_handler(event, context):
         buffer = io.BytesIO()
         resized_image.save(buffer, format=image.format)
         buffer.seek(0)
-
-        # Saving the scaled image in the S3 destination bucket
-        s3_client.put_object(Body=object_content, Bucket=dest_bucket_name, Key=f'resized/{object_key}' )
+        
+        # Saving the scaled image in the destination bucket
+        s3_client.put_object(Body=buffer, Bucket=dest_bucket_name, Key=f'resized/{object_key}')
 
         return {
             'statusCode': 200,
-            'body': json.dumps('succesfully scaled image')
+            'body': json.dumps('Successfully scaled image')
         }
-
+        
     except Exception as e:
-        print(f'Error {e}')
+        print(f'Error: {e}')
         return {
-            'statusCode': 200,
+            'statusCode': 500,
             'body': json.dumps('Error scaling image')
         }
